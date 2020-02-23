d54a58b0d8c45af2ff8585ab9d02184285bbfd69
Graeme Clark <grae0026@stud.kea.dk>
2020-02-23 14:06:34 +0100

    clean students from JSON  and create prototype object

--- script.js
+++ script.js
@@ -1,56 +1,115 @@
 window.addEventListener("DOMContentLoaded", getStudents);
 
-// fetch students from API
+// global array for fixed students
+const allStudents = [];
+
+// object prototype for fixed students
+const studentName = {
+  firstName: "",
+  lastName: "",
+  middleName: "",
+  nickName: "",
+  house: "",
+  image: null
+};
+
+// function fetch students from API
 function getStudents() {
-  fetch("json/students1991.json")
+  // console.log("getStudents");
+  fetch("https://petlatkea.dk/2020/hogwarts/students.json")
     .then(res => res.json())
-    .then(loadStudents);
+    .then(fixStudents);
 }
 
-function loadStudents(studentNames) {
-  studentNames.forEach(function(student) {
-    const studentList = document.querySelector(".student_list").content;
-    const studentCopy = studentList.cloneNode(true);
-    const singleStudent = studentCopy.querySelector(".single_student");
-    const link = singleStudent.querySelector(".name");
+// function to clean up student list
+function fixStudents(studentList) {
+  // console.log("fixStudents");
+  studentList.forEach(jsonObject => {
+    // console.log(jsonObject);
 
-    link.textContent = student.fullname;
-    link.setAttribute("name", student.fullname);
-    link.setAttribute("house", student.house);
-    link.onclick = function() {
-      showDetails(this);
-      console.log(student.house);
-    };
+    // create new student object from 'studentName' prototype
+    let student = Object.create(studentName);
 
-    // append copy to section
-    document.querySelector("#student_section").appendChild(studentCopy);
+    // for full name - trim whitespace, change to lowercase, replace characters, split at spaces
+    let fullname = jsonObject.fullname
+      .trim()
+      .toLowerCase()
+      .replace(/[-""]/g, " ")
+      .split(" ");
+
+    // for house - trim whitespace and change to lowercase
+    let house = jsonObject.house.trim().toLowerCase();
+
+    // capitalize first letters of house and first name
+    student.house = capitalizeFirstLetter(house);
+    student.firstName = capitalizeFirstLetter(fullname[0]);
+
+    // if full name is equal to 2 strings - capitalize first letter of 2nd string
+    if (fullname.length == 2) {
+      student.lastName = capitalizeFirstLetter(fullname[1]);
+
+      // if full name is equal to 3 strings - capitalize first letter of 2nd and 3rd strings
+    } else if (fullname.length == 3) {
+      student.lastName = capitalizeFirstLetter(fullname[1]);
+      student.middleName = capitalizeFirstLetter(fullname[2]);
+
+      // if full name is equal to 4 strings - capitalize first letter of 2nd, 3rd and 4th strings
+    } else if (fullname.length == 4) {
+      student.middleName = capitalizeFirstLetter(fullname[1]);
+      student.lastName = capitalizeFirstLetter(fullname[2]);
+      student.nickName = capitalizeFirstLetter(fullname[3]);
+    }
+
+    // add student image
+    student.image = "http://www.lovethatwillnotdie.com/avatars/" + student.lastName.toLowerCase() + "_" + student.firstName[0].toLowerCase() + ".png";
+
+    // add fixed student to allStudents array
+    allStudents.push(student);
+    // console.log(student);
   });
+
+  // show fixed students
+  allStudents.forEach(showStudents);
 }
 
-// select theme
-document.querySelector("select#theme").addEventListener("change", selected);
-
-// change theme
-function selected() {
-  const selectedTheme = this.value;
-  document.querySelector(".modal_content").style.setProperty("background-color", "var(--theme-" + this.value + ")");
-  document.querySelector(".modal_name").style.setProperty("font-size", "100px");
+//capitalize first letter
+function capitalizeFirstLetter(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
-// the modal information
-function showDetails(data) {
-  modal.querySelector(".modal_name").textContent = data.getAttribute("name");
-  modal.querySelector(".modal_house").textContent = data.getAttribute("house");
-  modal.style.display = "block";
+// show students
+function showStudents(studentName) {
+  // console.log("showStudents");
+  const template = document.querySelector("template").content;
+  const studentCopy = template.cloneNode(true);
+  const modal = document.getElementById("myModal");
+  studentCopy.querySelector(".first_name").textContent = studentName.firstName;
+  studentCopy.querySelector(".middle_name").textContent = studentName.middleName;
+  studentCopy.querySelector(".last_name").textContent = studentName.nickName;
+  studentCopy.querySelector(".nick_name").textContent = studentName.lastName;
+  studentCopy.querySelector(".house").textContent = studentName.house;
+  studentCopy.querySelector(".student_image").src = studentName.image;
+  studentCopy.querySelector("button").addEventListener("click", () => {
+    modal.classList.remove("hide");
+    modal.style.display = "block";
+    modal.querySelector(".modal_first_name").textContent = studentName.firstName;
+    modal.querySelector(".modal_middle_name").textContent = studentName.middleName;
+    modal.querySelector(".modal_last_name").textContent = studentName.lastName;
+    modal.querySelector(".modal_nick_name").textContent = studentName.nickName;
+    modal.querySelector(".modal_house").textContent = studentName.house;
+    modal.querySelector(".modal_content").dataset.theme = studentName.house;
+  });
+  document.querySelector("#student_section").appendChild(studentCopy);
 }
 
 // get modal
 const modal = document.getElementById("myModal");
+modal.addEventListener("click", () => {
+  modal.classList.add("hide");
+});
 
 // get element that closes the modal
 const span = document.getElementsByClassName("close")[0];
-
-// close modal - click x
 span.onclick = function() {
   modal.style.display = "none";
 };
